41678390b0e78d43588aee3ffd2a76507cd9cc4e
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-05 13:15:28 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -11,7 +11,7 @@ class DelayedButton extends React.Component {
   }
 
   render() {
-    return <button onClick={this.handleClick}>Delayed</button>
+    return <button onClick={this.handleClick}>Delay</button>
   }
 
 }
